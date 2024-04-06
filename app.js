5dbe70ed49206427e07d5e7c8d95a628e77c1058
Ashley Chiteri <ochamiashley97@gmail.com>
2024-04-07 01:52:25 +0300

    returned to previous

--- app.js
+++ app.js
@@ -31,18 +31,3 @@ about.addEventListener('click', () =>{
 contacts.addEventListener('click', () =>{
     scrollToElement('.header', 3);
 });
-
-/*sending emails
-function sendEmail(){
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "ashleychiteri@gmail.com",
-        Password : "09FF3C2C3367BB3DED31A3EAE5DE7F7E0B8E",
-        To : 'ochamiashley97@gmail.com',
-        From : document.getElementById("email").value,
-        Subject : "New Contact Form Enquiry",
-        Body : "And this is the body"
-    }).then(
-      message => alert(message)
-    );
-}*/
